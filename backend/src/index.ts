7a8a6615fc726b586f36c93a23fd10a280809191
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-18 01:36:55 -0400

    Install ts-node-dev

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3333);
