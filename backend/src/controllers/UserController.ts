69ee52139c8d8d071398f45cc14a35753605e535
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-18 02:02:19 -0400

    Create

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 
+import EmailService from '../services/EmailService';
+
 const users = [
   { name: 'André', lastName: 'Rodrigues', email: 'dev-asantos@outlook.com' },
 ];
@@ -7,5 +9,23 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail(
+      {
+        name: 'André',
+        lastName: 'Rodrigues',
+        email: 'dev-asantos@outlook.com',
+      },
+      {
+        subject: 'Bem-vindo ao sistema',
+        body: 'Seja bem-vindo',
+      },
+    );
+
+    return res.send('');
   },
 };
