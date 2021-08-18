69a2a74cd25977b42ad32d773ec7d5dce5ca0b34
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-18 15:51:13 -0400

    GetData and spread users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import User from './components/User';
+
+import api from './services/api';
+
+type TUser = {
+  name: string;
+  lastName: string;
+  email: string;
+};
+
 function App() {
-  return <div className='App'></div>;
+  const [users, setUsers] = useState<TUser[]>([]);
+
+  useEffect(() => {
+    api.get<TUser[]>('/users').then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>Users</h1>
+
+      <section>
+        {users.map((user) =>
+          user.name === 'André' ? (
+            <User user={user}>Sou eu!</User>
+          ) : (
+            <User user={user} />
+          ),
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default App;
